cb39bddc767976332cbca1c9154617e250810184
tiimwqq <tiimwqq@gmail.com>
2024-02-26 20:21:27 +0600

    2 lesson

--- script.js
+++ script.js
@@ -22,6 +22,17 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+// 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+// 2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+// отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+// возвращаем пользователя к вопросам опять
+
+// 3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+// "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+// "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+// 4) Потренироваться и переписать цикл еще двумя способами*/
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
@@ -34,14 +45,93 @@ const personalMovieDB = {
     privat: false
 }
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies.a = b;
-personalMovieDB.movies.c = d;
+    if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done')
+    } else {
+        console.log('error');
+        i--;
+    }
+
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count < 30 && personalMovieDB.count > 10) {
+        alert('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        alert('Вы киноман');
+    } else {
+        alert('error');
+    }
+
+}
 
 console.log(personalMovieDB);
+
+
+let num = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    console.log(num);
+
+}
+
+showFirstMessage('hello world');
+console.log(num);
+
+
+let lines = 30;
+let result = '';
+for (let i = 0; i < lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += ' ';
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += '*';
+    }
+    result += '\n';
+} console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
